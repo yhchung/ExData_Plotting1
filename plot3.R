e2b87fe4c9c56594555a0b25b0c4436ef7d819c5
Younghyun Chung <younghyun.chung@gmail.com>
2014-06-07 15:16:21 -0400

    minor change
    for legend

--- plot3.R
+++ plot3.R
@@ -1,6 +1,6 @@
 # a data loading function is in a seperate R file
-source("loading_power_data.R")
-data <- loadData()
+#source("loading_power_data.R")
+#data <- loadData()
 
 dev.new()
 par(bg="transparent")
@@ -10,7 +10,7 @@ with(data, {
              xlab="", ylab="Energy sub metering")
         lines(DateTime, Sub_metering_2, col="red")
         lines(DateTime, Sub_metering_3, col="blue")
-        legend("topright", lty=1,
+        legend("topright",  lty=1, lwd=1, cex=0.8, y.intersp = 0.6,
                legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
                col=c("black", "red", "blue"))
         
@@ -18,5 +18,5 @@ with(data, {
 
 dev.copy(png, filename="plot3.png", width=480, height=480) # PNG file output
 dev.off()
-dev.off()
+#dev.off()
 
